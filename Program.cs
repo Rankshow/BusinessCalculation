8792c0b6144f9199260c9a4e9dafe2870c0c3323
Rankshow <uwaomaobinna20@gmail.com>
2023-09-10 09:16:16 +0100

    Using long process to calculate netIncome

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BusinessCalculation;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ! This will render the netIncome, NB: we are using the long process in this case.
+        Console.WriteLine("***Let's start by calculating the NetIncome of the company**!!!");
+        Console.WriteLine("What is the Revenue?");
+        string? Revenue = Console.ReadLine();
+        double revenue = Convert.ToDouble(Revenue);
+
+        Console.WriteLine("What is the expenses?");
+        string? Expenses = Console.ReadLine();
+        double expense = Convert.ToDouble(Expenses);
+
+        double netIncome = NetIncome(revenue, expense);
+
+        Console.WriteLine($"The NetIncome of the company is {netIncome}");
+
+
+    }
+
+    // * This will rhe inherit at the top-level code.
+       private static double NetIncome(double revenue, double expenses)
+       {
+         double total = revenue - expenses;
+         return total;
+       }
+
+       private static double TotalAsset(double liability, double equity)
+       {
+         double result = liability + equity;
+         return result;
+       }
+}
