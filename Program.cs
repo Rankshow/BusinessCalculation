7de872e9ac0246fd5befde2f172f52ca50f249a7
Rankshow <uwaomaobinna20@gmail.com>
2023-09-11 14:02:15 +0100

    added some condition that control the netincome processes

--- Program.cs
+++ Program.cs
@@ -4,36 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        // ! This will render the netIncome, NB: we are using the long process in this case.
-        Console.WriteLine("***Let's start by calculating the NetIncome of the company**!!!");
-        Console.WriteLine("What is the Revenue?");
-        string? Revenue = Console.ReadLine();
-        double revenue = Convert.ToDouble(Revenue);
+        Console.WriteLine("What is your name?");
+        string? name = Console.ReadLine();
+        Console.WriteLine($"Good Afternoon! {name}, your are welcome to our company.");
+        Console.WriteLine($"Please, {name} kindly choose your favourite country you would like to visit.");
+        Console.WriteLine("1. Africa \n2. Europe");
+        string? country = Console.ReadLine();
 
-        Console.WriteLine("What is the expenses?");
-        string? Expenses = Console.ReadLine();
-        double expense = Convert.ToDouble(Expenses);
-
-        double netIncome = NetIncome(revenue, expense);
-
-        // * Controlling the code base with a conditional statement
-        if (netIncome <= 10)
+        if (country == "1")  
         {
-        Console.WriteLine($"The NetIncome of the company is {netIncome} which very low");
+          ProcessNetIncome();
         }
-        else 
+        else
         {
-        Console.WriteLine($"The NetIncome of the company is {netIncome} which is fine");
+          ProcessTotalAsset();
         }
+
     }
 
-    // * This will the inherit at the top-level code.
+      // ! processNetIncome
+      private static void ProcessNetIncome()
+      {
+        Console.WriteLine("Please provide your revenue data");
+        double revenue = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Please Provide your expenses data");
+        double expenses = Convert.ToDouble(Console.ReadLine()); 
+
+        double result = NetIncome(revenue, expenses);
+
+        if (result < 500)
+        {
+        Console.WriteLine($"Please, the company total Net income is {result} which is very poor, we need to find a lasting solution.");
+        }  
+        else
+        {
+        Console.WriteLine($"The company net income is {result} is good very fine.");
+        }      
+
+      }
+      // ! ProcessTotalAsset
+      private static void ProcessTotalAsset()
+      {
+        Console.WriteLine("Please provide your liability data");
+        double liability = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Please Provide your equity data");
+        double equity = Convert.ToDouble(Console.ReadLine()); 
+
+        double answer = NetIncome(liability, equity);
+        Console.WriteLine($"Your total Net income is {answer}.");
+
+      }
+
+    // ***NetIncome****.
        private static double NetIncome(double revenue, double expenses)
        {
          double total = revenue - expenses;
          return total;
        }
 
+    // ***TotalAsset****.
        private static double TotalAsset(double liability, double equity)
        {
          double result = liability + equity;
